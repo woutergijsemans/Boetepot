036d1d7a137232d8592b05b27cb8fea0e98d4cd1
ATOMIUM\WGSAI67 <wouter.gijsemans@realdolmen.com>
2022-12-09 13:09:30 +0100

    update api calls

--- src/Client/src/app/team/team.service.ts
+++ src/Client/src/app/team/team.service.ts
@@ -7,12 +7,9 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class TeamService {
-
-  private baseUrl: string = 'api/boetepot/';
   constructor(private http: HttpClient) { }
 
   public getBySlug(slug: string): Observable<any> {
-    //return this.http.get<any>(`api/boetepot/teams/${slug}`);
-    return this.http.get<any>(`${this.baseUrl}teams/${slug}`);
+    return this.http.get<any>(`${environment.apiBaseUrl}teams/${slug}`);
   }
 }
--- src/Client/src/environments/environment.prod.ts
+++ src/Client/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiBaseUrl: 'https://thebarn-2288.azurewebsites.net/api/boetepot/'
 };
